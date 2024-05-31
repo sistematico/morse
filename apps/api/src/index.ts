c410e7da1c9dd387b0f7eaa26d6fb06e94cfaa1f
Lucas Saliés Brum <sistematico@gmail.com>
2024-05-31 04:37:53 -0400

    Update

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,38 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
+app.use('*', cors())
+
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Morse API')
 })
 
-export default app
+const server = Bun.serve<{ socketId: number }>({
+  fetch(req, server) {
+    if (server.upgrade(req, { data: { socketId: Math.random() } })) {
+      return
+    }
+    return app.fetch(req)
+  },
+  websocket: {
+    open(ws) {
+      const socketId = Math.random()
+      ws.data = { socketId } // Inicializa ws.data
+      //addClient(socketId, ws)
+      console.log(`WebSocket connection opened: ${socketId}`)
+    },
+    message(ws, message) {
+      console.log(`Received ${message} from ${ws.data.socketId}`)
+      //broadcastMessage(String(message))
+    },
+    close(ws) {
+      //removeClient(ws.data.socketId)
+      console.log(`WebSocket connection closed: ${ws.data.socketId}`)
+    }
+  },
+  port: 4010
+})
+
+console.log(`Server running at http://localhost:${server.port}`)
